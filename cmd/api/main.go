3ff941962b07502d364c2fef4c676a226b9ca72a
Kinshuk Goel <kinshukgoel@Kinshuks-MacBook-Air.local>
2024-08-06 22:52:49 +0530

    86cw5jq9p: http server setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"context"
 	"epifigo/connection"
 	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
 	fmt.Println("Welcome to EPIFIGO")
-	connection.ConnectToMongo()
+	mongoClient, err := connection.ConnectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
 
+	defer func() {
+		if err = mongoClient.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	log.Println("Server running in port", 8080)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
